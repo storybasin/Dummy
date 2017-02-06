c0cf434206002d407c7d6369e1194954ad25e929
storybasin <storybasin88@naver.com>
2017-02-06 14:17:14 +0900

    remote repository add a HelloGitHub.java

---
+++ HelloGitHub/src/com/sist/HelloGitHub.java
@@ -0,0 +1,11 @@
+package com.sist;
+
+public class HelloGitHub {
+	public static void main (String[] args){
+		System.out.println("Hello Git");
+		System.out.println("What's Git");
+		System.out.println("It's DVCS");
+		System.out.println("It's collaboration");
+		System.out.println("Test");
+	}
+}
